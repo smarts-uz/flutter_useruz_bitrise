8c14f9530fa1d8b308674eda2cf1b279a41d477d
JasurbekErgashev <jergashev633@gmail.com>
2023-07-20 14:54:35 +0500

    changes

--- lib/src/ui/main/search/chat/chat_item_screen.dart
+++ lib/src/ui/main/search/chat/chat_item_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_build_context_synchronously, deprecated_member_use
+// ignore_for_file: use_build_context_synchronously, deprecated_member_use, prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:io';
 
